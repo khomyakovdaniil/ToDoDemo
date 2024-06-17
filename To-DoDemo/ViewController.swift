73ade8acc134ea050ff1b6dbb2ab404e1ad50574
KhomyakovDaniil <daniil.h@appomart.com>
2024-06-17 14:06:31 +0300

    Initial Commit

---
+++ To-DoDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To-DoDemo
+//
+//  Created by  Даниил Хомяков on 17.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
